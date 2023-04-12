3d67d059a4d0960d9c654dd5a3eef9b273dcd5b3
jemeryhsiao <1402223078xxj@gmail.com>
2023-04-12 16:19:04 +0800

    log:`again`

--- a/main.go
+++ a/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	poscom := poscom.NewGainscha("GFTF57YONDFPJU2G1DV004YZTMD9LJAO", "FC2A29F38B18C33C745504C3F6F668AD")
-	fmt.Println(poscom.Device().DeviceList())
-	fmt.Println(poscom.Setting().SetPushUrl("1111"))
+	gainscha := poscom.NewGainscha("GFTF57YONDFPJU2G1DV004YZTMD9LJAO", "FC2A29F38B18C33C745504C3F6F668AD")
+	fmt.Println(gainscha.Device().DeviceList())
+	fmt.Println(gainscha.Setting().SetPushUrl("1111"))
 }
